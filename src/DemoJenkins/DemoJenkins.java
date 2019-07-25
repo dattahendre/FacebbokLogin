52bd31cd41fcf48c27b38fba1e488ce0582d6152
Aarya <Aarya@Datta>
2019-07-25 13:27:25 +0530

    hi...

---
+++ src/DemoJenkins/DemoJenkins.java
@@ -0,0 +1,22 @@
+package DemoJenkins;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+	public static WebDriver driver;
+	@Test
+	public void OpenApplication()
+	{
+		driver=new FirefoxDriver();
+		driver.get("http://facebook.com");
+		
+		System.out.println("welcome to My jenkins world");
+		driver.close();
+		System.out.println("hello aarya");
+		driver.quit();
+		driver.close();
+	}
+
+}
